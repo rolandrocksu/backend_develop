afc6bb334cb997eff922e2479347ed519a66bc33
Roland <rolandandrolandand@gmail.com>
2022-08-31 13:55:46 +0400

    Created account app and account model

--- marketer_space/account/admin.py
+++ marketer_space/account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Account
+
+
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    save_as = True
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
--- marketer_space/account/models.py
+++ marketer_space/account/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Account(AbstractUser):
+    ACCOUNT_TYPE = (
+        ('super_admin', 'Super Admin'),
+        ('organization_admin', 'Organization Admin'),
+        ('user', 'User')
+    )
+
+    country = models.CharField(max_length=128)
+    organization = models.ForeignKey(
+        'Organization', on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    profile_picture = models.ImageField(upload_to='profile/%Y/%m')
+    role = models.CharField(
+        max_length=45,
+        choices=ACCOUNT_TYPE,
+        default=ACCOUNT_TYPE[2]
+    )
+
+    creation_date = models.DateTimeField(auto_now=True)
+    modification_date = models.DateTimeField(null=True)
+
+
+class Organization(models.Model):
+    domain = models.CharField(max_length=256)
+    name = models.CharField(max_length=128)
+
